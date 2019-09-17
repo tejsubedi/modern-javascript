ada87fa7474b91b3df3ecfa2bac2fc7b1cf7905f
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-17 18:24:15 -0400

    learn diff between var and const

---
+++ index.js
@@ -0,0 +1,18 @@
+//old school way
+var name = "Tej";
+name = "Raj";
+alert(name);
+
+//print output Tej ...Raj
+
+const name = "Tej";
+
+alert(name);
+//alert name = Tej
+
+const name = "Tej";
+name ="Raj";
+alert(name);
+//Throw error
+
+
